ede2fcbb893770db45e07661dd11ff553f7d505c
HARIVARDANI <130393562+HARIVARDANI@users.noreply.github.com>
2024-09-24 09:57:09 +0530

    JWT Added

---
+++ CodingChallenge/src/main/java/com/springboot/codingChallenge/Controller/UserController.java
@@ -0,0 +1,39 @@
+package com.springboot.codingChallenge.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.codingChallenge.Model.UserInfo;
+import com.springboot.codingChallenge.Repository.UserRepository;
+
+
+@RestController
+ public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+    @GetMapping("/user/hello")
+    public String userHello() {
+        return "Hello, User!";
+    }
+ 
+    @PostMapping("/auth/signup")
+    public void signup(@RequestBody UserInfo userInfo) {
+    	userInfo.setRole("ROLE_ADMIN"); 
+    	userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+    	userRepository.save(userInfo);
+    }
+ 
+    @GetMapping("/admin/hello")
+    public String adminHello() {
+        return "Hello, Admin!";
+    }
+}
---
+++ CodingChallenge/src/main/java/com/springboot/codingChallenge/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.springboot.codingChallenge.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import com.springboot.codingChallenge.Model.UserInfo;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserInfo, Long> {
+    Optional<UserInfo> findByUsername(String username);
+}
